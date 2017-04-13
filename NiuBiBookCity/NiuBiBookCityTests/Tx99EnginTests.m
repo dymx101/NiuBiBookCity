413a915e76f773e5cbdb83d7935255df6089de55
Yiming <Yiming.Dong@agoda.com>
2017-04-13 14:53:36 +0700

    unit test

--- NiuBiBookCity/NiuBiBookCityTests/Tx99EnginTests.m
+++ NiuBiBookCity/NiuBiBookCityTests/Tx99EnginTests.m
@@ -11,6 +11,10 @@
 #import "BMFramework.h"
 #import "DefineHeader.h"
 #import "BCTBookModel.h"
+#import "BCTBookChapterModel.h"
+
+static const CGFloat unitTestTimeout = 10;
+static const BOOL unitTestEnableLogging = true;
 
 @interface Tx99EnginTests : XCTestCase
 @property (nonatomic, strong) Txt99Engine *engine;
@@ -43,11 +47,14 @@
         
         if (intError == 0) {
             
-            NSArray *books = weakBaseParam.resultArray;
-            if ([books.firstObject isKindOfClass:[BCTBookModel class]]) {
-                NSString *link = ((BCTBookModel *)books.firstObject).bookLink;
-                NSLog(@"Book Link: %@", link);
+            if (unitTestEnableLogging) {
+                NSArray *books = weakBaseParam.resultArray;
+                if ([books.firstObject isKindOfClass:[BCTBookModel class]]) {
+                    NSString *link = ((BCTBookModel *)books.firstObject).bookLink;
+                    NSLog(@"Book Link: %@", link);
+                }
             }
+            
             [expectation fulfill];
             
         }
@@ -55,16 +62,77 @@
     
     [self.engine getSearchBookResult:baseparam];
     
-    [self waitForExpectationsWithTimeout:10 handler:^(NSError * _Nullable error) {
+    [self waitForExpectationsWithTimeout:unitTestTimeout handler:^(NSError * _Nullable error) {
         if (error) {
             NSLog(@"%s Error: %@", __PRETTY_FUNCTION__, error);
         }
     }];
 }
 
-- (void)test {
-    @"http://www.txt99.cc/readbook/17068.html";
+- (void)testGetChapterList {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"testGetChapterList"];
+    
+    BMBaseParam* baseparam = [BMBaseParam new];
+    baseparam.paramString = @"http://www.txt99.cc/readbook/17068.html";
+    
+    __weak BMBaseParam *weakBaseParam = baseparam;
+    
+    baseparam.withresultobjectblock=^(int intError,NSString* strMsg,id obj) {
+        
+        if (intError == 0) {
+            
+            if (unitTestEnableLogging) {
+                NSArray *chapters = weakBaseParam.resultArray;
+                if ([chapters.firstObject isKindOfClass:[BCTBookChapterModel class]]) {
+                    BCTBookChapterModel *chapter = ((BCTBookChapterModel *)chapters.firstObject);
+                    NSLog(@"**Chapter** ->\n HostURL: %@ \nURL: %@", chapter.hostUrl, chapter.url);
+                }
+            }
+            
+            [expectation fulfill];
+        }
+        
+    };
+    
+    [self.engine getBookChapterList:baseparam];
+    
+    [self waitForExpectationsWithTimeout:unitTestTimeout handler:^(NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"%s Error: %@", __PRETTY_FUNCTION__, error);
+        }
+    }];
 }
 
+//HostURL:, URL:
+- (void)testGetBookChapterDetail {
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"testGetChapterList"];
+    
+    BMBaseParam* baseparam=[BMBaseParam new];
+    
+    //参数
+    baseparam.paramString = @"http://www.txt99.cc/";
+    baseparam.paramString2 = @"http://www.txt99.cc/readbook/17068_1.html";
+    
+    __weak BMBaseParam *weakBaseParam = baseparam;
+    baseparam.withresultobjectblock=^(int intError,NSString* strMsg,id obj) {
+        if (intError == 0) {
+            
+            if (unitTestEnableLogging) {
+                NSLog(@"Chapter Content: %@", weakBaseParam.resultString);
+            }
+            
+            [expectation fulfill];
+        }
+    };
+    
+    [self.engine getBookChapterDetail:baseparam];
+    
+    [self waitForExpectationsWithTimeout:unitTestTimeout handler:^(NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"%s Error: %@", __PRETTY_FUNCTION__, error);
+        }
+    }];
+}
 
 @end

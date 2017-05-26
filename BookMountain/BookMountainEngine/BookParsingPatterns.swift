86ddb9f0521426909f0da118208cb98852befa10
Yiming Dong <yimingdong@Yimings-MacBook-Pro.local>
2017-05-26 21:03:06 +0800

    complet patterns

--- BookMountain/BookMountainEngine/BookParsingPatterns.swift
+++ BookMountain/BookMountainEngine/BookParsingPatterns.swift
@@ -8,14 +8,25 @@
 
 import Foundation
 
-// Analyzing Txt99Engine
+/// BookParsingPatterns 用于配置图书解析时的正则表达式
 class BookParsingPatterns {
     
+    /// 用于解析出图书列表
     var listPattern: String?
     
+    /// 以下pattern用于解析出bookModel的信息
     var titlePattern: String?
     var bookLinkPattern: String?
     var authorPattern: String?
     var imageSrcPattern: String?
     var memoPattern: String?
+    
+    /// 章节容器pattern, 某些网站会在章节列表外面包一层
+    var chapterContainterPattern: String?
+    /// 用于从章节列表抓出一个章节
+    var chapterItemPattern: String?
+    /// 用于从章节string中解析出title和url
+    var chapterDetailPattern: String?
+    /// 用于解析出章节内容
+    var chapterContentPattern: String?
 }
